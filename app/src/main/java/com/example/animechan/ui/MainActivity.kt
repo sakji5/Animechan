8ba5b49a89ef42a1c0d78dbd55a8bfe688189677
Dmitry <antimoda17@gmail.com>
2022-07-10 19:14:35 +0500

    Added some ui

--- app/src/main/java/com/example/animechan/ui/MainActivity.kt
+++ app/src/main/java/com/example/animechan/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.animechan
+package com.example.animechan.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.animechan.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
